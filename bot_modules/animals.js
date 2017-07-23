c0df3699977627235774768d3ed600be8d9ace85
Meredith Espinosa <goclonefilms@gmail.com>
2017-07-23 03:18:28 -0700

    Added !bun to help file, removed Kamijo because Kamijo is not an animal
    (and *is* the icon of a TERF semi-cult)

--- bot_modules/animals.js
+++ bot_modules/animals.js
@@ -7,7 +7,7 @@ let Flickr = require("flickrapi");
 
 let help = "**Animals**\n";
 help += "Gets important pictures of important animals.\n";
-help += "*!shoob*, *!pangolin*, *!corgi*, *!duck*, *!goose*, *!seal*, *!opossum*.\n";
+help += "*!shoob*, *!pangolin*, *!corgi*, *!duck*, *!goose*, *!seal*, *!opossum*, *!bun*.\n";
 
 //** Set up Flickr api
 
@@ -15,51 +15,51 @@ let flickrOptions = {
     api_key: botConfig.moduleConfig.animals.flickrApikey,
     secret: botConfig.moduleConfig.animals.flickrSecretkey
   };
- 
+
 //** Command handlers
 
 let commandHandlers = {};
 
 commandHandlers.shoob = function (message, args) {
-  
+
   flickrRandomPhotoBySearch("samoyed", function (picture) {
-    
+
     if (picture) {
-      
+
       message.channel.send(picture);
-      
+
     }
-    
+
   });
-  
+
 };
 
 commandHandlers.pangolin = function (message, args) {
-  
+
   flickrRandomPhotoByGroup("974702@N22", function (picture) {
-    
+
     if (picture) {
-      
+
       message.channel.send(picture);
-      
+
     }
-    
+
   });
-  
+
 };
 
 commandHandlers.opossum = function (message, args) {
-  
+
   flickrRandomPhotoByGroup("16199074@N00", function (picture) {
-    
+
     if (picture) {
-      
+
       message.channel.send(picture);
-      
+
     }
-    
+
   });
-  
+
 };
 
 commandHandlers.bun = function (message, args) {
@@ -76,139 +76,125 @@ commandHandlers.bun = function (message, args) {
 
 };
 
-commandHandlers.kamijo = function (message, args) {
-  
-  flickrRandomPhotoBySearch("kamijo", function (picture) {
-    
-    if (picture) {
-      
-      message.channel.send(picture);
-      
-    }
-    
-  });
-  
-};
-
 commandHandlers.corgi = function (message, args) {
-  
+
   flickrRandomPhotoBySearch("corgi+dog", function (picture) {
-    
+
     if (picture) {
-      
+
       message.channel.send(picture);
-      
+
     }
-    
+
   });
-  
+
 };
 
 commandHandlers.duck = function (message, args) {
-  
+
   flickrRandomPhotoByGroup("16694458@N00", function (picture) {
-    
+
     if (picture) {
-      
+
       message.channel.send(picture);
-      
+
     }
-    
+
   });
-  
+
 };
 
 commandHandlers.seal = function (message, args) {
-  
+
   flickrRandomPhotoByGroup("18035618@N00", function (picture) {
-    
+
     if (picture) {
-      
+
       message.channel.send(picture);
-      
+
     }
-    
+
   });
-  
+
 };
 
 commandHandlers.goose = function (message, args) {
-  
+
   flickrRandomPhotoByGroup("979023@N22", function (picture) {
-    
+
     if (picture) {
-      
+
       message.channel.send(picture);
-      
+
     }
-    
+
   });
-  
+
 };
 
 let flickrRandomPhotoBySearch = function (term, callback) {
-  
+
   Flickr.tokenOnly(flickrOptions, function(error, flickr) {
-  
+
     flickr.photos.search({
       text: term,
       page: 1,
       per_page: 120,
       sort: "relevance"
     }, function(err, result) {
-      if(err) { 
-        
+      if(err) {
+
         console.log(error);
-        
+
         callback(false);
-      
+
       }
 
       if (!err) {
-        
+
         let picture = randomProperty(result.photos.photo);
 
         let pictureUrl = "https://farm" + picture.farm + ".staticflickr.com/" + picture.server + "/" + picture.id + "_" + picture.secret + ".jpg";
 
         callback(pictureUrl);
-        
+
       }
 
     });
-  
+
   });
-  
+
 };
 
 let flickrRandomPhotoByGroup = function (groupid, callback) {
-  
+
   Flickr.tokenOnly(flickrOptions, function(error, flickr) {
-  
+
     flickr.groups.pools.getPhotos({
       group_id: groupid
     }, function(err, result) {
-      if(err) { 
-        
+      if(err) {
+
         console.log(error);
-        
+
         callback(false);
-      
+
       }
 
       if (!err) {
-        
+
         let picture = randomProperty(result.photos.photo);
 
         let pictureUrl = "https://farm" + picture.farm + ".staticflickr.com/" + picture.server + "/" + picture.id + "_" + picture.secret + ".jpg";
 
         callback(pictureUrl);
-        
+
       }
 
     });
-  
+
   });
-  
+
 };
 
 let randomProperty = function (obj) {
